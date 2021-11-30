4edb52ed48671bc7839b8f9036169c10e197ada4
choi2601 <65222200+choi2601@users.noreply.github.com>
2021-11-30 19:12:24 +0900

    Add:CategoryBar add

--- libido-web-frontend/src/pages/Main/CategoryBar.js
+++ libido-web-frontend/src/pages/Main/CategoryBar.js
@@ -1,7 +1,45 @@
 import React from "react";
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
 
 const CategoryBar = () => {
-  return <div>CategoryBar</div>;
+  return (
+    <Container>
+      <CategoryList>
+        <MoveCategoryButton>LIBIDO</MoveCategoryButton>
+        <MoveCategoryButton className="noActive">TRENDING</MoveCategoryButton>
+        <MoveCategoryButton className="noActive">FRIENDS</MoveCategoryButton>
+      </CategoryList>
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  min-width: 1330px;
+  padding: 15px 400px;
+  border: 1px solid #979797;
+  border-radius: 100px;
+  background-color: #fff;
+`;
+
+const CategoryList = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+const MoveCategoryButton = styled.button`
+  border: none;
+  background: none;
+  font-size: 16px;
+  font-weight: 600;
+  color: #4dbce9;
+  cursor: pointer;
+
+  &.noActive {
+    //추후 동적 스타일 생성
+    color: #606770;
+  }
+`;
 
 export default CategoryBar;
--- libido-web-frontend/src/pages/Main/MainPage.js
+++ libido-web-frontend/src/pages/Main/MainPage.js
@@ -1,17 +1,25 @@
 import React from "react";
+import styled from "styled-components";
 import CategoryBar from "./CategoryBar";
 import MainTemplate from "./MainTemplate";
 import MainForm from "./MainForm";
 
 const MainPage = () => {
   return (
-    <>
+    <Container>
       <CategoryBar />
       <MainTemplate>
         <MainForm />
       </MainTemplate>
-    </>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  max-width: 1330px;
+  position: absolute;
+  top: 100px;
+  left: 92px;
+`;
 
 export default MainPage;
