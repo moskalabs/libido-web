3a50032c70c23cbab493efa76cac3823686d4999
choi2601 <65222200+choi2601@users.noreply.github.com>
2021-11-30 12:19:29 +0900

    Add: TopNav Layout 초기 세팅

--- libido-web-frontend/src/components/TopNav/TopNav.js
+++ libido-web-frontend/src/components/TopNav/TopNav.js
@@ -1,7 +1,37 @@
 import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 const TopNav = () => {
-  return <div>TopNav입니다.</div>;
+  return (
+    <Container>
+      <TopNavHeader>
+        <LogoContainer />
+        {/* <SearchBar /> */}
+        <RightSubMenu>
+          <MessageContainer>
+            <MessageLogo />
+            <MessageLink />
+          </MessageContainer>
+          {/* <Button /> */}
+        </RightSubMenu>
+      </TopNavHeader>
+    </Container>
+  );
 };
+
+const Container = styled.div``;
+
+const TopNavHeader = styled.header``;
+
+const LogoContainer = styled.div``;
+
+const RightSubMenu = styled.div``;
+
+const MessageContainer = styled.div``;
+
+const MessageLogo = styled.img``;
+
+const MessageLink = styled(Link)``;
 
 export default TopNav;
