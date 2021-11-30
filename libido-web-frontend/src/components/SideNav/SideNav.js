b14eba68c6582d9e622fb977ac15bf0fce4316a0
choi2601 <65222200+choi2601@users.noreply.github.com>
2021-11-30 17:50:20 +0900

    Add:SideNav Layout 초기 생성

--- libido-web-frontend/src/components/SideNav/SideNav.js
+++ libido-web-frontend/src/components/SideNav/SideNav.js
@@ -1,7 +1,81 @@
 import React from "react";
+import styled from "styled-components";
 
 const SideNav = () => {
-  return <div>SideNav</div>;
+  return (
+    <Container>
+      <SideNavHeader>
+        <ToggleButton>토글</ToggleButton>
+        <IconContainer>
+          <IconBox>
+            <Icon data-button="home">버튼</Icon>
+            <p>홈</p>
+          </IconBox>
+          <IconBox>
+            <Icon data-button="room">버튼</Icon>
+            <p>방만들기</p>
+          </IconBox>
+          <IconBox>
+            <Icon data-button="search">버튼</Icon>
+            <p>검색</p>
+          </IconBox>
+          <IconBox>
+            <Icon data-button="setting">버튼</Icon>
+            <p>설정</p>
+          </IconBox>
+        </IconContainer>
+      </SideNavHeader>
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  max-width: 46px;
+  height: 100%;
+  padding: 0 46px 0;
+  float: left;
+  background-color: blueviolet;
+`;
+
+const SideNavHeader = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 25px;
+`;
+
+const ToggleButton = styled.button`
+  border: none;
+  width: 50px;
+  height: 50px;
+  cursor: pointer;
+  background-color: burlywood;
+`;
+
+const IconContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 30px;
+`;
+
+const IconBox = styled.div`
+  margin-bottom: 20px;
+  text-align: center;
+
+  p {
+    margin-top: 3px;
+    font-size: 13px;
+    color: #707070;
+  }
+`;
+
+const Icon = styled.button`
+  border: none;
+  width: 50px;
+  height: 50px;
+`;
 
 export default SideNav;
