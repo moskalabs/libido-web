dcd8ed407cb8ebed42c01533ed93380ce66e70ac
choi2601 <65222200+choi2601@users.noreply.github.com>
2021-12-01 10:36:59 +0900

    Add: Container add

--- libido-web-frontend/src/pages/Main/MainTemplate.js
+++ libido-web-frontend/src/pages/Main/MainTemplate.js
@@ -1,7 +1,12 @@
 import React from "react";
+import styled from "styled-components";
 
 const MainTemplate = ({ children }) => {
-  return <div>{children}</div>;
+  return <Container>{children}</Container>;
 };
+
+const Container = styled.div`
+  max-width: 1330px;
+`;
 
 export default MainTemplate;
