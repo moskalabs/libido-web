509fb5b362d84ae04bf20f06bd26ceb98c08551c
choi2601 <65222200+choi2601@users.noreply.github.com>
2021-12-21 18:42:16 +0900

    Refactor: Search Bar 관련 이슈 해결

--- libido-web-frontend/src/components/common/SearchBar.js
+++ libido-web-frontend/src/components/common/SearchBar.js
@@ -37,7 +37,7 @@ const SearchBar = ({
       topNav={topNav}
       placeholder={placeholder}
       onChange={changeKeyword}
-      onKeyDown={searchKeyword}
+      onKeyPress={searchKeyword}
       value={keyword}
     />
   );
--- libido-web-frontend/src/lib/api/mainForm.js
+++ libido-web-frontend/src/lib/api/mainForm.js
@@ -2,23 +2,11 @@ import client from "./client";
 import { API } from "../../config";
 import { identifyQuerySort } from "../identifyQuerySort";
 
-// 서버에서 받아 온 데이터를 정제해주는 역할 필요(categorySort와 관련되어 분기 처리_title에 들어갈 내용 받아 온 데이터 추가적으로 기입)
-
-// export const content = ({ categorySort }) => {
-//   const querySort = identifyQuerySort(categorySort);
-//   client.get(`${API.content}${querySort}`);
-// };
-
-// export const rooms = ({ categorySort }) => {
-//   const querySort = identifyQuerySort(categorySort);
-//   client.get(`${API.rooms}${querySort}`);
-// };
-
 export const content = ({ sort, currentOffset = 1 }) => {
   const querySort = identifyQuerySort(sort);
-  console.log(currentOffset);
+
   return client.get(
-    `http://15.164.210.185:8000/contents?category=${querySort}&offset=${
+    `${API.baseUrl}contents?category=${querySort}&offset=${
       Math.abs(currentOffset - 1) * 8
     }`,
     {
@@ -28,20 +16,13 @@ export const content = ({ sort, currentOffset = 1 }) => {
       },
     }
   );
-  // return client.get(
-  //   `/data/${
-  //     querySort === "customize"
-  //       ? "contentCustomize.json"
-  //       : "contentPopular.json"
-  //   }`
-  // );
 };
 
 export const rooms = ({ sort, currentOffset = 1 }) => {
   const querySort = identifyQuerySort(sort);
-  // console.log(currentOffset);
+
   return client.get(
-    `http://15.164.210.185:8000/rooms?category=${querySort}&offset=${
+    `${API.baseUrl}rooms?category=${querySort}&offset=${
       Math.abs(currentOffset - 1) * 8
     }`,
     {
@@ -51,16 +32,10 @@ export const rooms = ({ sort, currentOffset = 1 }) => {
       },
     }
   );
-  // return client.get(
-  //   `/data/${
-  //     querySort === "customize" ? "roomsCustomize.json" : "roomsPopular.json"
-  //   }`
-  // );
 };
 
 export const friendList = () => {
-  // return client.get("/data/followFriends.json");
-  return client.get("http://15.164.210.185:8000/users/follows", {
+  return client.get(`${API.baseUrl}users/follows`, {
     headers: {
       Authorization:
         "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6NDF9.P5mvM9ULQ6qiuBL10ld6ZmilB349CHKfBc32gdzXqL4",
@@ -69,12 +44,8 @@ export const friendList = () => {
 };
 
 export const friends = (currentOffset = 1) => {
-  // return client.get("/data/friendsRoom.json");
-
   return client.get(
-    `http://15.164.210.185:8000/rooms/friends?offset=${
-      Math.abs(currentOffset - 1) * 8
-    }`,
+    `${API.baseUrl}rooms/friends?offset=${Math.abs(currentOffset - 1) * 8}`,
     {
       headers: {
         Authorization:
